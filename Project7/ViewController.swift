4f29120c25dfc9fc767220d88dde8f135d12bdeb
Yulian Gyurov <julian.gyuroff@gmail.com>
2023-10-10 21:01:39 +0300

    Modify project 7 so that your filtering code takes place in the background.

--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -27,22 +27,28 @@ class ViewController: UITableViewController {
             urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
         }
         
-        if let url = URL(string: urlString){
-            if let data = try? Data(contentsOf: url){
-                //Data fetched OK
-                //print("data.count=\(data.count)")
-                parse(json: data)
-                return
+        DispatchQueue.global(qos: .userInitiated).async {
+            [weak self] in
+            if let url = URL(string: urlString){
+                if let data = try? Data(contentsOf: url){
+                    //Data fetched OK
+                    //print("data.count=\(data.count)")
+                    self?.parse(json: data)
+                    return
+                }
             }
+            self?.showError()
         }
-        showError()
+      
      }
     
     func showError() {
-        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; Check your connection and try again.", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "OK", style: .default))
-        present(ac, animated: true)
-        
+        DispatchQueue.main.async {
+            [weak self] in
+            let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; Check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self?.present(ac, animated: true)
+        }
     }
 
     func parse(json: Data){
@@ -54,7 +60,11 @@ class ViewController: UITableViewController {
 //            for item in petitions{
 //                print("TITLE \(item.title)","BODY \(item.body)")
 //            }
-            tableView.reloadData()
+            DispatchQueue.main.async {
+                [weak self] in
+                self?.tableView.reloadData()
+            }
+            
         }
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -109,12 +119,21 @@ class ViewController: UITableViewController {
         
         filteredPetitions.removeAll(keepingCapacity: true)
         
-        for item in petitions{
-            if item.title.contains(searchText) || item.body.contains(searchText){
-                filteredPetitions.append(item)
+        DispatchQueue.global(qos:   .userInitiated).async {
+            [weak self] in
+            guard let tempPetitions = self?.petitions else { return }
+            for item in tempPetitions  {
+                if item.title.contains(searchText) || item.body.contains(searchText){
+                    self?.filteredPetitions.append(item)
+                }
             }
+            DispatchQueue.main.async {
+                [weak self] in
+                self?.tableView.reloadData()
+            }
+            
         }
-        tableView.reloadData()
+        
     }
 }
 
